69d244ef039dadcd585b8669a5fb3978b23b7df1
Ismael Ahumada <Ismael.Ahumada@solera.com>
2022-11-04 11:22:09 +0100

    Small change for Program.cs

--- Program.cs
+++ Program.cs
@@ -19,10 +19,10 @@ builder.Services.AddDbContext<ClientService>(mysqlBuilder => {
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapRazorPages();
 
 app.Run();
